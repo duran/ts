d96af533faa193355d1cb53dd7f1d7dd8b4a6faa
viric <viric@llimona>
2007-11-12 09:33:39 +0100

    Passing SIGINT to the child, if it started.

--- signals.c
+++ signals.c
@@ -5,10 +5,15 @@
     Please find the license in the provided COPYING file.
 */
 #include <signal.h>
+#include <stdio.h>
 #include <stdlib.h> /* for NULL */
+#include "main.h"
 
 /* Some externs refer to this variable */
 static sigset_t normal_sigmask;
+
+/* as extern in execute.c */
+int signals_child_pid; /* 0, not set. otherwise, set. */
 
 void ignore_sigpipe()
 {
@@ -24,3 +29,44 @@ void restore_sigmask()
     sigprocmask(SIG_SETMASK, &normal_sigmask, NULL);
 }
 
+void sigint_handler(int s)
+{
+    if (signals_child_pid)
+    {
+        kill(signals_child_pid, SIGINT);
+    } else
+    {
+        warning("ts client killed by SIGINT");
+        exit(1);
+    }
+}
+
+void block_sigint()
+{
+    sigset_t set;
+
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    /* ignore_sigpipe() will always be called first, and
+     * only that sets the normal_sigmask. */
+    sigprocmask(SIG_BLOCK, &set, 0);
+}
+
+void unblock_sigint_and_install_handler()
+{
+    sigset_t set;
+    struct sigaction act;
+
+    /* Install the handler */
+    act.sa_handler = sigint_handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGINT, &act, 0);
+
+    /* Unblock the signal */
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    /* ignore_sigpipe() will always be called first, and
+     * only that sets the normal_sigmask. */
+    sigprocmask(SIG_UNBLOCK, &set, 0);
+}
