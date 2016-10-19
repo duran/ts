bf403f863db718a3592f219c30b40c40fb3c023b
viric <viriketo@gmail.com>
2016-10-19 22:08:34 +0200

    Setting version to 1.0.0

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int server_socket;
 static char getopt_env[] = "POSIXLY_CORRECT=YES";
 static char *old_getopt_env;
 
-static char version[] = "Task Spooler v0.7.6 - a task queue system for the unix user.\n"
+static char version[] = "Task Spooler v1.0.0 - a task queue system for the unix user.\n"
 "Copyright (C) 2007-2016  Lluis Batlle i Rossell";
 
 
